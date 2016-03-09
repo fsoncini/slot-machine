8b743324cf4d3a213653313ab41f2bf9646d2753
Federico Soncini <fsonc002@gold.ac.uk>
2016-03-09 11:50:50 +0000

    Added Start Scene
    Plus tied up folders

---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class LevelManager : MonoBehaviour {
+
+    //Create function for loading level when Start button is pressed
+    const int max_count = 5;
+    int count = 0;
+    bool still_trying = true;
+
+    public void LoadLevel(string name){
+        Debug.Log("Level load requested for " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest() {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+ 
+}
